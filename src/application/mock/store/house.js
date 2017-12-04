0d1cbe7bfe4fe74df579528f9357f22c5628b02c
doudou <douyadong@wkzf.com>
2017-12-04 21:09:30 +0800

    12.04

---
+++ src/application/mock/store/house.js
@@ -0,0 +1,190 @@
+let apiData={
+  "message": "成功",
+  "status": 1,
+  "data": {
+    "rentHouseList": [
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "东城安定门",
+        "spaceArea": "100.00",
+        "houseTypeStr": "3室3厅2卫",
+        "rentPriceStr": "1000",
+        "district":"黄浦区",
+        "town":"老西门",
+        "houseTitle": "10AM新坐标古北路1000号",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "50f26c9331454884"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "长宁区古北区",
+        "spaceArea": "52.00",
+        "houseTypeStr": "0室0厅1卫",
+        "rentPriceStr": "1852",
+        "houseTitle": "金龙公寓",
+        "district":"黄浦区",
+        "town":"老西门",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "48610fea4d42a03a"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "松江区泗泾",
+        "spaceArea": "89.00",
+        "houseTypeStr": "1室0厅0卫",
+        "district":"黄浦区",
+        "town":"老西门",
+        "rentPriceStr": "2400",
+        "houseTitle": "金港花园",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "5581739bb4165ea1"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "松江区泗泾",
+        "spaceArea": "69.00",
+        "houseTypeStr": "1室1厅0卫",
+        "district":"黄浦区",
+        "town":"老西门",
+        "rentPriceStr": "2544",
+        "houseTitle": "金港花园",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "4e41529f6c3075d3"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "长宁区古北区",
+        "spaceArea": "58.00",
+        "houseTypeStr": "1室1厅1卫",
+        "district":"黄浦区",
+        "town":"老西门",
+        "rentPriceStr": "2500",
+        "houseTitle": "金马公寓",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "7b86d88add29bc50"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "海淀四季青",
+        "spaceArea": "25.00",
+        "houseTypeStr": "2室2厅1卫",
+        "rentPriceStr": "36",
+        "houseTitle": "WeHouse；WE常青园南里二区",
+        "district":"黄浦区",
+        "town":"老西门",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "3b3429188945c11a"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "龙华民治",
+        "spaceArea": "36.00",
+        "houseTypeStr": "2室2厅1卫",
+        "district":"黄浦区",
+        "town":"老西门",
+        "rentPriceStr": "36",
+        "houseTitle": "龙塘新村",
+        "houseTag": {
+          "isSubwayHouse": 0,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "3462834de59de09c"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "东城安定门",
+        "spaceArea": "100.00",
+        "district":"黄浦区",
+        "town":"老西门",
+        "houseTypeStr": "3室3厅2卫",
+        "rentPriceStr": "1000",
+        "houseTitle": "10AM新坐标古北路1000号",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "35d14b8690d6ed37"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "东城安定门",
+        "spaceArea": "100.00",
+        "district":"黄浦区",
+        "town":"老西门",
+        "houseTypeStr": "3室3厅2卫",
+        "rentPriceStr": "1000",
+        "houseTitle": "10AM新坐标古北路1000号",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "b2f4cd1cedea1400"
+      },
+      {
+        "firstImageUrl": "https://imgwater-test.oss.aliyuncs.com/6af6e136ce64436598f5e016bc9378f3.ML",
+        "districtAndTownName": "东城安定门",
+        "spaceArea": "100.00",
+        "district":"黄浦区",
+        "town":"老西门",
+        "houseTypeStr": "3室3厅2卫",
+        "rentPriceStr": "1000",
+        "houseTitle": "10AM新坐标古北路1000号",
+        "houseTag": {
+          "isSubwayHouse": 1,
+          "isPriceDown": 0,
+          "isNewHouse": 0,
+          "isHardcover": 0,
+          "isSouth": 0
+        },
+        "encryptHouseId": "5b7c7b4936bbbc7c"
+      }
+    ],
+    "total": 22
+  }
+}
+export default apiData;

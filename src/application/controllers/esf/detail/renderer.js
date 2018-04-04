245581d3e923709e55dfb6cad60a714bbffe79ce
451230805@qq.com <lance70451234>
2018-04-04 09:26:58 +0800

    测试二手房详情折线图

--- src/application/controllers/esf/detail/renderer.js
+++ src/application/controllers/esf/detail/renderer.js
@@ -54,7 +54,7 @@ class Renderer extends AppRendererControllerBasic {
             // 相似房源更多的Url
             item['similarHousesUrl'] = this.templateData.domain +'/esf/similarList.html?enCryptHouseId='+item.encryptHouseId;
             // 额外的脚本样式 折线图
-            let  extraJavascript = [this.templateData.utilStaticPrefix+'/wkzf/js/util/echarts/echarts.3.2.3.min.js'];
+            let  extraJavascript = [this.templateData.utilStaticPrefix+'/wkzf/js/util/echarts/echarts.js'];
             /*++-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
             相册的视频和图片的数据的组装处理
             -----------------------------------------------------------------------------------------------------------------------------------------------------------------------++*/
